9d5bc81ed9c8f4a2ec5204bc9d3688fee77c5ec3
DanyaErezer <pavlov.shura12@gmail.com>
2025-03-25 16:37:28 +0700

    feature: Создание CatStoreRequest и частичное заполнение контроллера CatController

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CatStoreRequest;
+use App\Models\Cat;
 use Illuminate\Http\Request;
 
 class CatController extends Controller
@@ -9,9 +11,22 @@ class CatController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = Cat::query();
+
+        if ($request->has('gender') && in_array($request->gender, ['Male', 'Female'])){
+            $query->where('gender', $request->gender);
+        }
+        if ($request->has('age_min')){
+            $query->where('age', '>=', $request->age_min);
+        }
+        if ($request->has('age_max')){
+            $query->where('age', '<=', $request->age_max);
+        }
+        $cats = $query->paginate(20);
+
+        return view('cats.index', compact('cats'));
     }
 
     /**
@@ -19,13 +34,16 @@ class CatController extends Controller
      */
     public function create()
     {
-        //
+        $mothers = Cat::where('gender', 'Female')->get();
+        $fathers = Cat::where('gender', 'Male')->get();
+
+        return view('cats.create', compact('mothers', 'fathers'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CatStoreRequest $request)
     {
         //
     }
