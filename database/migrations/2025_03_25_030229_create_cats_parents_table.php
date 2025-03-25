85d4bd2a8be02a1b2fa6b42220f128a47f80bbbc
DanyaErezer <pavlov.shura12@gmail.com>
2025-03-25 10:30:38 +0700

    feature:Создание БД и миграций

---
+++ database/migrations/2025_03_25_030229_create_cats_parents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cats_parents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kitten_id')->constrained('cats')->onDelete('cascade');
+            $table->foreignId('mother_id')->constrained('cats')->onDelete('cascade');
+            $table->foreignId('father_id')->constrained('cats')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cats_parents');
+    }
+};
