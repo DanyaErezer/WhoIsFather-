c81f5f25da08f331c3eff27235d10a4d6ca079a9
DanyaErezer <pavlov.shura12@gmail.com>
2025-03-25 10:49:03 +0700

    feature:Заполнение моделей

--- app/Models/Cat.php
+++ app/Models/Cat.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cat extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name', 'gender', 'age'];
+
+    public function catMom()
+    {
+        return $this->hasMany(CatsParent::class, 'mother_id');
+    }
+    public function catFather(){
+        return $this->hasMany(CatsParent::class, 'father_id');
+    }
+    public function catParent(){
+        return $this->hasMany(CatsParent::class, 'parent_id');
+    }
+    public function mother(){
+        return $this->hasManyThrough(
+            CatsParent::class,
+            CatsParent::class,
+            'kitten_id',
+            'id',
+            'id',
+            'mother_id');
+    }
+    public function father(){
+        return $this->hasManyThrough(
+            CatsParent::class,
+            CatsParent::class,
+            'kitten_id',
+            'id',
+            'id',
+            'father_id');
+    }
+
 }
--- app/Models/CatsParent.php
+++ app/Models/CatsParent.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CatsParent extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['kitten_id', 'mother_id', 'father_id'];
+
+    public function kitten()
+    {
+        return $this->BelongsTo(Cat::class, 'kitten_id');
+    }
+
+    public function mother()
+    {
+        return $this->BelongsTo(Cat::class, 'mother_id');
+    }
+    public function father()
+    {
+        return $this->BelongsTo(Cat::class, 'father_id');
+    }
+
 }
